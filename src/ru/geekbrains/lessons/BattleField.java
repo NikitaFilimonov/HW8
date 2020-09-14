9f8b4c21e7ccd1ce69b9eae24d958f8c8e874981
NikitaFilimonov <69960509+NikitaFilimonov@users.noreply.github.com>
2020-09-14 22:25:33 +0300

    HW8

---
+++ src/ru/geekbrains/lessons/BattleField.java
@@ -0,0 +1,103 @@
+package ru.geekbrains.lessons;
+
+import sun.rmi.runtime.Log;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.Locale;
+
+public class BattleField extends JPanel {
+    private GameWindow gameWindow;
+
+    private int mode;
+    private int fieldSize;
+    private int winningLength;
+
+    private boolean isInit;
+
+    int cellWidth;
+    int cellHeight;
+
+    public BattleField(GameWindow gameWindow) {
+        this.gameWindow = gameWindow; //передаем ссылку на класс gameWindow
+
+        setBackground(Color.ORANGE);
+
+        addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseReleased(MouseEvent e) {
+                int cellX = e.getX() / cellWidth;
+                int cellY = e.getY() / cellHeight;
+
+//                System.out.println(cellY + " " + cellX);
+
+                if (!Logic.isFinishedGame) {
+                    Logic.humanTurn(cellX, cellY);
+
+//                    if (Logic.isFinishedGame){
+//                        //result
+//                    } для ДЗ
+                }
+
+                repaint();
+            }
+        });
+    }
+
+    public void startNewGame(int mode, int fieldSize, int winningLength) {
+        this.mode = mode;
+        this.fieldSize = fieldSize;
+        this.winningLength = winningLength;
+
+        isInit = true;
+
+        repaint();
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        if (!isInit) {
+            return;
+        }
+
+        cellWidth = getWidth() / fieldSize;
+        cellHeight = getHeight() / fieldSize;
+
+        for (int i = 0; i < fieldSize; i++) {
+            int y = i * cellHeight;
+            g.drawLine(0, y, getWidth(), y);
+        }
+
+        for (int i = 0; i < fieldSize; i++) {
+            int x = i * cellWidth;
+            g.drawLine(x, 0, x, getHeight());
+        }
+
+        for (int i = 0; i < Logic.SIZE; i++) {
+            for (int j = 0; j < Logic.SIZE; j++) {
+                if (Logic.map[i][j] == Logic.DOT_X) {
+                    drawX(g, j, i);
+                }
+                if (Logic.map[i][j] == Logic.DOT_O) {
+                    drawO(g, j, i);
+                }
+            }
+
+        }
+
+    }
+
+    private void drawO(Graphics g, int cellX, int cellY) {
+    }
+
+    private void drawX(Graphics g, int cellX, int cellY) {
+        ((Graphics2D) g).setStroke(new BasicStroke(5));//ширина линии
+        g.setColor(Color.BLUE);
+        g.drawLine(cellX * cellWidth, cellY * cellHeight,
+                (cellX + 1) * cellWidth, (cellY + 1) * cellHeight);
+    }
+}
---
+++ src/ru/geekbrains/lessons/Main.java
@@ -0,0 +1,7 @@
+package ru.geekbrains.lessons;
+
+public class Main {
+    public static void main(String[] args) {
+        new GameWindow();
+    }
+}
